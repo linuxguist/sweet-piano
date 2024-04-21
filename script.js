e89e50210b3a77a561aa3f7d2c19f05b2f97bae6
nathan <linuxguist@gmail.com>
2024-04-21 16:40:10 +0530

    no message

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+$(window).keydown(function (event) {
+    if (event.keyCode == 111) {
+        event.preventDefault();
+        return false;
+    }
+});
+*/
+
+
+function playSound(e) {
+
+    if (event.keyCode == 111) {
+        event.preventDefault();
+        return false;
+    }
+  
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const curKey = document.querySelector(`.wkey[data-key="${e.keyCode}"],.bkey[data-key="${e.keyCode}"]`);
+
+  if (!audio) return; // stop the function from running altogether 
+  audio.currentTime = 0; // rewind to the start
+
+  audio.play();
+  curKey.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // skip if not transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.wkey, .bkey');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
